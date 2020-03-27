6d2792292ae27ba40476dbc0789e6b1cbeeeffcf
Kis Balázs <kisbalazs90@gmail.com>
2020-03-27 11:32:32 +0100

    Connect to db

---
+++ src/config/db.ts
@@ -0,0 +1,38 @@
+import * as Knex from 'knex'
+import { Logger } from '@overnightjs/logger';
+
+export const config = {
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  },
+  migrations: {
+    // This is missing from the TypeScript types currently.
+    stub: 'migration.stub'
+  }
+}
+
+const instance: Knex = Knex(config as Knex.Config)
+
+Logger.Info(
+  `Will connect to postgres://${config.connection.user}@${
+    config.connection.host
+  }/${config.connection.database}`
+)
+instance
+  .raw('select 1')
+  .then(() => {
+    Logger.Info(`Connected to database - OK`)
+  })
+  .catch(err => {
+    Logger.Err(`Failed to connect to database: ${err}`)
+    process.exit(1)
+  })
+
+export const db = () => instance
+
+// Returns a timestamp suitable for inserting into Postgres
+export const timestamp = (): string => new Date().toUTCString()
